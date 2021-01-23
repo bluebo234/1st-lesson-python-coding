6a3c4473d465894190dc10c07fcfd651aac38f76
bluebo234 <liethan625@gmail.com>
2021-01-23 20:19:40 +0000

    play around strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+a = 'Hello'
+b = "hello"
+c = """hello
+Next Line
+3rd licence
+                            ----again
+        a little shorter
+"""
+print(c) 
+name = input("What is your name?\n:")
+print(c.upper())
+print(c.capitalize())
+print(b * 10)
