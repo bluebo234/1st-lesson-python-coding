daceacb9a7e108046b022c2fe2a7764e91b2e603
bluebo234 <liethan625@gmail.com>
2021-01-24 14:55:43 +0000

    added fibbonaci

---
+++ homework.py
@@ -0,0 +1,11 @@
+print((102/2)*100/2)
+a = 1
+b = 2
+c = 0
+print(a)
+print(b)
+while c < 100:
+ c = a + b
+ print(c)
+ a = b
+ b = c
---
+++ types.py
@@ -0,0 +1,19 @@
+a = '123'
+b = int(a)
+print("a = ", a)
+print("b = ", b)
+
+print(35 / 2)
+print(35 // 2)
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+who = input("What are you? ")
+if who == "dog":
+  print("woof woof")
+elif who == "cat":
+  print("meow")
+else:
+  print("I dont know you")
+print("press enter to exit")
