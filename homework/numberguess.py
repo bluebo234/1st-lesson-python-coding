8b76e0a755d5027774445b7c3de1ddbb35928b89
bluebo234 <liethan625@gmail.com>
2021-01-26 23:14:32 +0000

    finished number guess game

--- homework/numberguess.py
+++ homework/numberguess.py
@@ -1,6 +1,13 @@
 import random
 rn = random.randint(1, 100)
-guess = input("I have chosen a number between 1 and 100. You have to type and guess the number. I will tell you if the number is higher or lower than the random number.")
+guess = int(input("I have chosen a number between 1 and 100. You have to type and guess the number. I will tell you if the number is higher or lower than the random number. "))
 while guess != rn:
   if guess > rn:
-    print()
+    print("The number I am thinking of is lower than", guess,)
+  elif guess < rn:
+    print("the number I am thinking of is higher than", guess,)
+  else:
+    print("Congratulations! The number was", rn,)
+  guess = int(input("Guess another number. "))
+print("Congratulations! The number was", rn,"!")
+print("press enter to exit")
